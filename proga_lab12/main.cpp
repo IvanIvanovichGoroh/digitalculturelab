23fbbc8936bb2f818ba27b24b093ae7ae45e4c5c
RustamOper05 <sahabutdinov200@gmail.com>
2021-11-27 15:00:00 +0300

    lab_dc_v0.12

---
+++ proga_lab12/main.cpp
@@ -0,0 +1,17 @@
+//12
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    FILE *file;
+    file = fopen("fscanf.txt", "w");
+    c = a + b;
+    fprintf(file, "%d", c);
+}
